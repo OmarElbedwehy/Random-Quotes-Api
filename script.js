6a24a2e0dd307d1e1c040dbea8f9da91d184b507
Omar Elbedwehy <98553227+OmarElbedwehy@users.noreply.github.com>
2023-01-31 21:41:00 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+let quote_btn = document.querySelector("#quote_btn");
+
+quote_btn.addEventListener("click", ()=>{
+    quote_btn.textContent = "Loading Quote...";
+    quote_btn.style.opacity = "0.6";
+    fetch("https://api.quotable.io/random").then((result) => {
+        let Data = result.json();
+        return Data
+    }).then((quote) => {
+        document.querySelector("#quotes").textContent = quote.content;
+        document.querySelector("#author").innerHTML = `<span>__</span>${quote.author}`;
+        quote_btn.textContent = "New Quote";
+        quote_btn.style.opacity = "1";
+    })
+})
+
+let speak = document.querySelector("#speak");
+
+speak.addEventListener("click", ()=>{
+    let quote_text = document.querySelector("#quotes").innerHTML;
+    let speech = new SpeechSynthesisUtterance(`${quote_text}`);
+    speechSynthesis.speak(speech)
+})
+
+let copy_btn = document.querySelector("#copy");
+
+copy_btn.addEventListener("click", ()=>{
+    let quote_text = document.querySelector("#quotes").innerHTML;
+    navigator.clipboard.writeText(quote_text)
+})
+
+let tweet_btn = document.querySelector("#twitter");
+
+tweet_btn.addEventListener("click", ()=>{
+    let quote_text = document.querySelector("#quotes").innerHTML;
+    let tweet_url = `https://twitter.com/intent/tweet?url=${quote_text}`;
+    window.open(tweet_url, "_blank")
+})
