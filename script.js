021094854b128507d010f02f118f446b7bd8479d
Omar Elbedwehy <98553227+OmarElbedwehy@users.noreply.github.com>
2023-02-01 00:19:53 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
+fetch("https://api.quotable.io/random").then((result) => {
+        let Data = result.json();
+        return Data
+    }).then((quote) => {
+        document.querySelector("#quotes").textContent = quote.content;
+        document.querySelector("#author").innerHTML = `<span>__</span>${quote.author}`;
+        quote_btn.textContent = "New Quote";
+        quote_btn.style.opacity = "1";
+        if (document.querySelector("#quotes").innerHTML == "") {
+            document.querySelector("#quotes").innerHTML = "Proplem Happened Can't Connect With Server You Can Refresh The Page And Try Again";
+            document.querySelector("#author").style.display = "none";
+            quote_btn.textContent = "Loading Quote...";
+            quote_btn.style.opacity = "0.6";
+        } else {
+            document.querySelector("#quotes").textContent = quote.content;
+            document.querySelector("#author").innerHTML = `<span>__</span>${quote.author}`;
+            quote_btn.textContent = "New Quote";
+            quote_btn.style.opacity = "1";
+        }
+})
+
 let quote_btn = document.querySelector("#quote_btn");
 
-quote_btn.addEventListener("click", ()=>{
+quote_btn.addEventListener("click", () => {
     quote_btn.textContent = "Loading Quote...";
     quote_btn.style.opacity = "0.6";
     fetch("https://api.quotable.io/random").then((result) => {
@@ -11,16 +32,23 @@ quote_btn.addEventListener("click", ()=>{
         document.querySelector("#author").innerHTML = `<span>__</span>${quote.author}`;
         quote_btn.textContent = "New Quote";
         quote_btn.style.opacity = "1";
-    }).catch(
-        setTimeout(()=>{
-            alert("Error: Proplem Happend Can't Connect With Server")
-        }, 1000)
-    )
+        if (document.querySelector("#quotes").innerHTML == "") {
+            document.querySelector("#quotes").innerHTML = "Proplem Happened Can't Connect With Server You Can Refresh The Page And Try Again";
+            document.querySelector("#author").style.display = "none";
+            quote_btn.textContent = "Loading Quote...";
+            quote_btn.style.opacity = "0.6";
+        } else {
+            document.querySelector("#quotes").textContent = quote.content;
+            document.querySelector("#author").innerHTML = `<span>__</span>${quote.author}`;
+            quote_btn.textContent = "New Quote";
+            quote_btn.style.opacity = "1";
+        }
+    })
 })
 
 let speak = document.querySelector("#speak");
 
-speak.addEventListener("click", ()=>{
+speak.addEventListener("click", () => {
     let quote_text = document.querySelector("#quotes").innerHTML;
     let speech = new SpeechSynthesisUtterance(`${quote_text}`);
     speechSynthesis.speak(speech)
@@ -28,14 +56,14 @@ speak.addEventListener("click", ()=>{
 
 let copy_btn = document.querySelector("#copy");
 
-copy_btn.addEventListener("click", ()=>{
+copy_btn.addEventListener("click", () => {
     let quote_text = document.querySelector("#quotes").innerHTML;
     navigator.clipboard.writeText(quote_text)
 })
 
 let tweet_btn = document.querySelector("#twitter");
 
-tweet_btn.addEventListener("click", ()=>{
+tweet_btn.addEventListener("click", () => {
     let quote_text = document.querySelector("#quotes").innerHTML;
     let tweet_url = `https://twitter.com/intent/tweet?url=${quote_text}`;
     window.open(tweet_url, "_blank");
